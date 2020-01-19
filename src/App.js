81c8e0b7b410cf9e187052e83782aed05e376ad2
Pedro Calogero <calogero.pvj@gmail.com>
2020-01-19 13:30:18 -0300

    structure cleaned

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React, {useState} from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { Hidden, Button, TextField } from "@material-ui/core";
+import Api from "./providers/api";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
+  
+
+  const [torrentData, setTorrentData] = useState({});
+
+
+  const handleRefreshData = async () => {
+    const {data} = await Api.getTorrentData();
+  };
+
+  const handleLogin = () => {
+
+  };
+
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <Grid container>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>
+          <TextField id="username" label="Standard" />
+          <TextField id="password" label="Filled" />
+          <Button onClick={handleRefreshData()}>Refresh</Button>
+          <Button onClick={handleLogin()}>Login</Button>
+        </Paper>
+      </Grid>
+      <Hidden xsDown>
+        <Grid item sm={4} md={3}>
+          <Paper className={classes.paper}>sideMenu</Paper>
+        </Grid>
+      </Hidden>
+      <Grid item xs={12} sm={8} md={9}>
+        <Paper className={classes.paper}>{torrentData}</Paper>
+      </Grid>
+    </Grid>
   );
 }
 
---
+++ src/providers/api.js
@@ -0,0 +1,23 @@
+import Axios from "axios";
+
+const Api = {
+    cookie: "",
+    url: "http://localhost:1234",
+    getTorrentData: async () => {
+        await Axios.get()
+
+
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+export default Api;
